4b01efb5a98455baa657754f0e8464f9ed0eec09
Szewczus <ewus9999@gmail.com>
2020-10-31 13:05:38 +0100

    Wykonanie instrukcji z lab 4 razem z poprawieniem kursora tak, aby zawsze po usunięciu znajdował sie na koncu linijki i nie wracal do polozenia na poczatek wiersza, poprawnienie programu tak zeby mozna bylo zstapic piersza cyfre

---
+++ app/src/main/java/com/example/myfrags/FragsData.java
@@ -0,0 +1,7 @@
+package com.example.myfrags;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+public class FragsData extends  ViewModel{
+    public final MutableLiveData<Integer> counter=new MutableLiveData<>(0);  //wartość we ViewModel (przechowywana dla wszsytkich fragmentów)
+}
